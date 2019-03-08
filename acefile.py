ec4df4197835b8301d66110d2e2545c0365631e0
Daniel Roethlisberger <daniel@roe.ch>
2019-03-08 19:14:33 +0100

    Use double quoted backslashes in doctest

--- acefile.py
+++ acefile.py
@@ -2719,8 +2719,8 @@ class AceMember:
         'helloworld'
         >>> AceMember._sanitize_filename(b'.././.././.././.././../etc/../')
         ''
-        >>> AceMember._sanitize_filename(b'c:\\c:\\CVE-2018-20250\\poc.lnk')
-        'c/c/CVE-2018-20250/poc.lnk'
+        >>> AceMember._sanitize_filename(b'c:\\\\c:\\\\CVE-2018-20250\\\\p.lnk')
+        'c/c/CVE-2018-20250/p.lnk'
         """
         filename = filename.decode('utf-8', errors='replace')
         # treat null byte as filename terminator
